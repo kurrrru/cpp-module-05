d1c060130d2a6c8cbca35f9840e3d8f5b8f771c6
kurrrru <kurrrru8850@gmail.com>
2025-07-23 11:36:22 +0900

    complete ex00

---
+++ ex00/BureaucratExceptions.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <sstream>
+
+#include "ex00/Bureaucrat.hpp"
+#include "toolbox/color.hpp"
+#include "toolbox/stepmark.hpp"
+
+Bureaucrat::GradeInvalidException::GradeInvalidException(
+    const std::string& message) : _message(message) {
+    std::stringstream ss;
+    ss << "Bureaucrat::GradeInvalidException created: \"" << message << "\"";
+    toolbox::logger::StepMark::notice(ss.str());
+}
+
+const char* Bureaucrat::GradeInvalidException::what() const throw() {
+    std::stringstream ss;
+    ss << "Bureaucrat::GradeInvalidException:what called: \""
+        << _message << "\"";
+    toolbox::logger::StepMark::debug(ss.str());
+    return _message.c_str();
+}
+
+Bureaucrat::GradeInvalidException::~GradeInvalidException() throw() {
+    std::stringstream ss;
+    ss << "Bureaucrat::GradeInvalidException destroyed: \"" << _message << "\"";
+    toolbox::logger::StepMark::debug(ss.str());
+}
+
+Bureaucrat::GradeTooHighException::GradeTooHighException(
+    const std::string& message) : GradeInvalidException(message) {
+    std::stringstream ss;
+    ss << "Bureaucrat::GradeTooHighException created: \"" << message << "\"";
+    toolbox::logger::StepMark::warning(ss.str());
+}
+
+Bureaucrat::GradeTooHighException::~GradeTooHighException()
+    throw() {
+    std::stringstream ss;
+    ss << "Bureaucrat::GradeTooHighException destroyed";
+    toolbox::logger::StepMark::info(ss.str());
+}
+
+Bureaucrat::GradeTooLowException::GradeTooLowException(
+    const std::string& message) : GradeInvalidException(message) {
+    std::stringstream ss;
+    ss << "Bureaucrat::GradeTooLowException created: \"" << message << "\"";
+    toolbox::logger::StepMark::warning(ss.str());
+}
+
+Bureaucrat::GradeTooLowException::~GradeTooLowException() throw() {
+    std::stringstream ss;
+    ss << "Bureaucrat::GradeTooLowException destroyed";
+    toolbox::logger::StepMark::info(ss.str());
+}
